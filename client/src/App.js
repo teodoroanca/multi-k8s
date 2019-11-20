c27812d3be1906a14f59a960e191af5be01fadf9
Teodor <oanca@gw20e.com>
2019-11-20 23:39:10 +0100

    sal

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator kubernetess
+            Fib calculator kubernetes sal 
           </a>
           <Link to="/">
             Homeeee
